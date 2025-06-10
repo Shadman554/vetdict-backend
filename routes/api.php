73ccb02a841fc3951129fb89983f77e38a25b90e
shadman1234456 <101889846+shadman1234456@users.noreply.github.com>
2025-06-10 16:44:42 +0300

    Fix CORS configuration and proxy settings

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::get('/test-db', function () {
     }
 });
 
-// Public routes
+// Public routes that don't require authentication
 Route::apiResource('words', \App\Http\Controllers\API\WordController::class)->only(['index', 'show']);
 Route::apiResource('diseases', \App\Http\Controllers\API\DiseaseController::class)->only(['index', 'show']);
 Route::apiResource('drugs', \App\Http\Controllers\API\DrugController::class)->only(['index', 'show']);
@@ -61,12 +61,33 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return $request->user();
     });
     
-    // Protected resource routes with all methods
-    Route::apiResource('words', \App\Http\Controllers\API\WordController::class)->except(['index', 'show']);
-    Route::apiResource('diseases', \App\Http\Controllers\API\DiseaseController::class)->except(['index', 'show']);
-    Route::apiResource('drugs', \App\Http\Controllers\API\DrugController::class)->except(['index', 'show']);
-    Route::apiResource('books', \App\Http\Controllers\API\BookController::class)->except(['index', 'show']);
-    Route::apiResource('normal-ranges', \App\Http\Controllers\API\NormalRangeController::class)->except(['index', 'show']);
-    Route::apiResource('staff', \App\Http\Controllers\API\StaffController::class)->except(['index', 'show']);
-    Route::apiResource('tutorial-videos', \App\Http\Controllers\API\TutorialVideoController::class)->except(['index', 'show']);
+    // Protected resource routes with all methods except index and show
+    Route::post('words', [\App\Http\Controllers\API\WordController::class, 'store'])->name('words.store');
+    Route::put('words/{word}', [\App\Http\Controllers\API\WordController::class, 'update'])->name('words.update');
+    Route::delete('words/{word}', [\App\Http\Controllers\API\WordController::class, 'destroy'])->name('words.destroy');
+    
+    // Repeat for other resources if needed
+    Route::post('diseases', [\App\Http\Controllers\API\DiseaseController::class, 'store'])->name('diseases.store');
+    Route::put('diseases/{disease}', [\App\Http\Controllers\API\DiseaseController::class, 'update'])->name('diseases.update');
+    Route::delete('diseases/{disease}', [\App\Http\Controllers\API\DiseaseController::class, 'destroy'])->name('diseases.destroy');
+    
+    Route::post('drugs', [\App\Http\Controllers\API\DrugController::class, 'store'])->name('drugs.store');
+    Route::put('drugs/{drug}', [\App\Http\Controllers\API\DrugController::class, 'update'])->name('drugs.update');
+    Route::delete('drugs/{drug}', [\App\Http\Controllers\API\DrugController::class, 'destroy'])->name('drugs.destroy');
+    
+    Route::post('books', [\App\Http\Controllers\API\BookController::class, 'store'])->name('books.store');
+    Route::put('books/{book}', [\App\Http\Controllers\API\BookController::class, 'update'])->name('books.update');
+    Route::delete('books/{book}', [\App\Http\Controllers\API\BookController::class, 'destroy'])->name('books.destroy');
+    
+    Route::post('normal-ranges', [\App\Http\Controllers\API\NormalRangeController::class, 'store'])->name('normal-ranges.store');
+    Route::put('normal-ranges/{normal_range}', [\App\Http\Controllers\API\NormalRangeController::class, 'update'])->name('normal-ranges.update');
+    Route::delete('normal-ranges/{normal_range}', [\App\Http\Controllers\API\NormalRangeController::class, 'destroy'])->name('normal-ranges.destroy');
+    
+    Route::post('staff', [\App\Http\Controllers\API\StaffController::class, 'store'])->name('staff.store');
+    Route::put('staff/{staff}', [\App\Http\Controllers\API\StaffController::class, 'update'])->name('staff.update');
+    Route::delete('staff/{staff}', [\App\Http\Controllers\API\StaffController::class, 'destroy'])->name('staff.destroy');
+    
+    Route::post('tutorial-videos', [\App\Http\Controllers\API\TutorialVideoController::class, 'store'])->name('tutorial-videos.store');
+    Route::put('tutorial-videos/{tutorial_video}', [\App\Http\Controllers\API\TutorialVideoController::class, 'update'])->name('tutorial-videos.update');
+    Route::delete('tutorial-videos/{tutorial_video}', [\App\Http\Controllers\API\TutorialVideoController::class, 'destroy'])->name('tutorial-videos.destroy');
 });
